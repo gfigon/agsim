7046478c23ccf79a098d6b281527dd9c4a3c1fca
gfigon <grzegorz.figon@gmail.com>
2024-03-18 11:07:04 +0100

    Minor changes, test file and docs.

--- R/sim_jobs_nr.R
+++ R/sim_jobs_nr.R
@@ -1,4 +1,14 @@
+#' Title
+#'
+#' @param n_sim Number of months to simulate.
+#' @param jobs_mean_month Mean jobs per month.
+#'
+#' @return A numeric vector of n_sim size.
+#' @export
+#'
+#' @examples
+#' sim_jobs_nr(60, 20)
 sim_jobs_nr <- function(n_sim, jobs_mean_month){
-  rpois(n_sim, jobs_mean_month)
+  stats::rpois(n_sim, jobs_mean_month)
 
 }
---
+++ tests/testthat/test-sim_jobs_nr.R
@@ -0,0 +1,3 @@
+test_that("nr of sims are correct", {
+  expect_equal(length(sim_jobs_nr(12, 20)), 12)
+})
